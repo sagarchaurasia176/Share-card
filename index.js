a6495f7bd0d3745965386b4a19ed13ad57cbc48b
sagar387 <101509099+sagarchaurasia176@users.noreply.github.com>
2023-07-22 16:19:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const modalsBox=document.getElementById('modalBox');
+// const closeBox=document.getElementById('closed');
+function openBox(){
+    modalsBox.classList.add("modalBox");
+}
+
+function closebar(){
+    modalsBox.classList.remove("modalBox");
+}
+ 
